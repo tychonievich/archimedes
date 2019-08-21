eca126b9b3a006c329d410db10d567f0ae81a62e
cr4bd <cr4bd@virginia.edu>
2019-08-21 16:54:18 -0400

    display feedback for graded, due-but-not-closed assignments

--- task.php
+++ task.php
@@ -447,7 +447,7 @@ echo '</div>';
 
 
 // display feedback
-if ((($close < $now) || ($isstaff && $isself))
+if ((($due < $now) || ($isstaff && $isself))
 && array_key_exists('grade', $details)
 && !array_key_exists('.ext-req', $details)) {
     grader_fb($details);
