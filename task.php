a9970a6298f3a7e1c55eadc051e34ba3935bf83a
cr4bd <cr4bd@virginia.edu>
2020-04-12 14:19:54 -0400

    do not multiply negative grades; take three

--- task.php
+++ task.php
@@ -282,12 +282,12 @@ function show_grade($gradeobj) {
         _show_grade_obj_row($ans, $gradeobj['.sub']['portion'], $gradeobj['.sub']['comments'], true, '- ');
         $score -= $gradeobj['.sub']['portion'];
     }
-    if (array_key_exists('.mult', $gradeobj)) {
+    if (array_key_exists('.mult', $gradeobj) && $score > 0.0) {
         // (with multiplier)
         _show_grade_obj_row($ans, $gradeobj['.mult']['ratio'], $gradeobj['.mult']['comments'], true, '× ');
         $score *= $gradeobj['.mult']['ratio'];
     }
-    if (array_key_exists('.adjustment', $gradeobj)) {
+    if (array_key_exists('.adjustment', $gradeobj) && $score > 0.0) {
         // (with multiplier)
         _show_grade_obj_row($ans, $gradeobj['.adjustment']['mult'], $gradeobj['.adjustment']['comments'], true, '× ');
         $score *= $gradeobj['.adjustment']['mult'];
